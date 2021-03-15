0c9e382bafbf048a4808a4965f43ce0e08d94dda
imithu <imithu@imithu.com>
2021-03-15 22:48:46 +0600

    Deleted - Login, Logout, Improved - Registner, Added - Authentication

--- UM/User/Register.php
+++ UM/User/Register.php
@@ -17,166 +17,172 @@ class Register
 {
 
 
-    /**
-     * register a user
-     * 
-     * @param string $username
-     * @param string $email
-     * @param string $password
-     * 
-     * @param string $usertype
-     * @param string $userstatus
-     * 
-     * 
-     * @return array $SR - i.  [ 'error'=>true,  'username_error'=>(bool), 'email_error'=>(bool) ]
-     *                     ii. [ 'error'=>false, 'temp_otp'=>'value', 'user_id'=>value ]
-     * 
-     * 
-     * @since   1.0.0
-     * @version 1.2.0
-     * @author  Mahmudul Hasan Mithu
-     */
-    public static function register_main( string $username, string $email, string $password, string $usertype, string $userstatus )
-    {
-        /**
-         * check any account exist or not based on username and email
-         * also check username syntax is valid or not
-         * and take action
-         * 
-         * @param string $username
-         * @param string $email
-         * 
-         * 
-         * @return array $SR [ $username_error=>(bool),    $email_error=>(bool) ]
-         * 
-         * @since   1.0.0
-         * @version 1.2.0
-         * @author  Mahmudul Hasan Mithu
-         */
-        $account_check = function( string $username, string $email )
-        {
-            $username_error = false;
-            $email_error = false;
-            
+  /**
+   * register a user
+   * 
+   * @param string $username
+   * @param string $email
+   * @param string $password
+   * 
+   * @param string $usertype
+   * 
+   * 
+   * @return string (json) $SR - i.   { "error": true, "username_error": (boolean), "email_error": (boolean) }
+   *                             ii.  { "error": false, 'temp_otp'=>'value', 'user_id'=>value }
+   * 
+   * 
+   * @since   1.0.0
+   * @version 1.7.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function register_main( string $username, string $email, string $password, string $usertype )
+  {
+    $SR =
+    [
+      'error' => true,
+      'username_error'=> true,
+      'email_error'=>true
+    ];
 
+    // removed side space except password
+    $username = htmlspecialchars(strtolower(trim($username)));    // convert the username to lowercase
+    $email = htmlspecialchars(strtolower(trim($email)));          // convert the email    to lowercase
+    $usertype = trim($usertype);
 
-            // check username syntax is okay or not
-            if(Syntax::username($username)){
-                // check username exist or not in database
-                $user_id = Users::id_username( $username );
-                if( $user_id>0 ){
-                    if( User::user_is_verified( $user_id ) ){
-                        $username_error = true;
-                    }else{
-                        Account::delete( $user_id );
-                    }
-                }
-            }else{
-                $username_error = true;
-            }
-            
+    $SR_temp = Register_Main_Func_Helper::account_check( $username, $email );
 
-            // check email exist or not in database
-            $user_id = Users::id_email( $email );
-            if( $user_id>0 ){
-                if( User::user_is_verified( $user_id ) ){
-                    $email_error = true;
-                }else{
-                    Account::delete( $user_id );
-                }
-            }
-            
-
-
-            $SR = 
-            [
-                'username_error' => $username_error,
-                'email_error' => $email_error
-            ];
-            return $SR;
-        };
-
-
-        /**
-         * create account
-         * 
-         * @param string $username
-         * @param string $email
-         * @param string $password
-         * 
-         * @param string $usertype
-         * @param string $userstatus
-         * 
-         * @return array $SR [ $temp_otp=>'value',    $user_id=>value ]
-         * 
-         * @since   1.0.0
-         * @version 1.2.0
-         * @author  Mahmudul Hasan Mithu
-         */
-        $account_create = function( string $username, string $email, string $password, string $usertype, string $userstatus  )
-        {
-            date_default_timezone_set('UTC');
-            $password = password_hash( $password, PASSWORD_DEFAULT );
-            $temp_otp = Unknown_Data::random_name();
-            $datetime = date('Y-m-d H:i:s'); 
-
-            DB::insert
-            ( 
-                'INSERT INTO UM_users (`id`, `username`, `email`, `password`, `usertype`, `userstatus`, `email_is_verified`, `temp_otp`, `datetime` ) VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?) ',
-                                //    [ $username, $email,  $password,   $usertype,  $userstatus,         'no',         $temp_otp,  $datetime ] 
-                                   [ htmlspecialchars($username), htmlspecialchars($email),  $password,   $usertype,  $userstatus,         'no',         $temp_otp,  $datetime ]
-            );
-
-            $user_id = Users::id_username( $username );
-
-
-            $SR = 
-            [
-                'temp_otp'=> $temp_otp,
-                'user_id'=> $user_id
-            ];
-            return $SR;
-            
-        };
-
-
-
-
-        $SR = 
-        [
-            'error' => true,
-            'username_error'=> true,
-            'email_error'=>true
-        ];
-
-        // removed side space except password
-        $username = strtolower(trim($username));    // convert the username to lowercase
-        $email = strtolower(trim($email));          // convert the email    to lowercase
-        $usertype = trim($usertype);
-        $userstatus = trim($userstatus);
-
-        $SR_temp = $account_check( $username, $email );
-
-        if( $SR_temp['username_error']==true || $SR_temp['email_error']==true ){
-            $SR =
-            [
-                'error' => true,
-                'username_error'=> $SR_temp['username_error'],
-                'email_error'=>$SR_temp['email_error']
-            ];
-            return $SR;
-        }
-
-        $SR_temp = $account_create( $username, $email, $password, $usertype, $userstatus );
-
-        $SR = 
-        [
-            'error' => false,
-            'temp_otp'=> $SR_temp['temp_otp'],
-            'user_id'=> $SR_temp['user_id']
-        ];
-
-
-        return $SR;
+    if( $SR_temp['username_error']===true || $SR_temp['email_error']===true ){
+      $SR =
+      [
+        'error' => true,
+        'username_error'=> $SR_temp['username_error'],
+        'email_error'=>$SR_temp['email_error']
+      ];
+      return json_encode($SR);
     }
+
+    $SR_temp = Register_Main_Func_Helper::create_account( $username, $email, $password, $usertype );
+    $SR = 
+    [
+      'error' => false,
+      'user_id'=> $SR_temp['user_id'],
+      'temp_otp'=> $SR_temp['temp_otp']
+    ];
+
+
+    return json_encode($SR);
+  }
+}
+
+
+
+
+final class Register_Main_Func_Helper
+{
+  /**
+   * create account
+   * 
+   * @param string $username
+   * @param string $email
+   * @param string $password
+   * 
+   * @param string $usertype
+   * 
+   * @return array $SR [ $temp_otp=>'value',    $user_id=>value ]
+   * 
+   * @since   1.0.0
+   * @version 1.7.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function create_account( string $username, string $email, string $password, string $usertype )
+  {
+    date_default_timezone_set('UTC');
+    $password = password_hash( $password, PASSWORD_DEFAULT );
+    $temp_otp = Unknown_Data::random_name();
+
+    DB::table('UM_users')->insert(
+      [
+        'id' => NULL,
+        'username' => $username,
+        'email' => $email,
+        'password' => $password,
+        'usertype' => $usertype,
+        'userstatus' => 'pending',
+        'email_is_verified' => 'no',
+        'temp_otp' => $temp_otp,
+        'datetime' => date('Y-m-d H:i:s')
+      ]
+    );
+
+    $user_id = Users::id_username( $username );
+
+
+    return
+    [
+        'temp_otp'=> $temp_otp,
+        'user_id'=> $user_id
+    ];
+  }
+
+
+  /**
+   * check any account exist or not based on username and email
+   * also check username syntax is valid or not
+   * and take action
+   * 
+   * @param string $username
+   * @param string $email
+   * 
+   * 
+   * @return array $SR [ $username_error=>(bool),    $email_error=>(bool) ]
+   * 
+   * @since   1.0.0
+   * @version 1.7.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function account_check( string $username, string $email )
+  {
+    $username_error = false;
+    $email_error = false;
+
+
+
+
+    // check username syntax is okay or not
+    if(Syntax::username($username)){
+      // check username exist or not in database
+      $user_id = Users::id_username( $username );
+      if( $user_id>0 ){
+        if( User::user_is_verified( $user_id ) ){
+          $username_error = true;
+        }else{
+          Account::delete( $user_id );
+        }
+      }
+    }else{
+      $username_error = true;
+    }
+
+
+    // check email exist or not in database
+    $user_id = Users::id_email( $email );
+    if( $user_id>0 ){
+      if( User::user_is_verified( $user_id ) ){
+        $email_error = true;
+      }else{
+         Account::delete( $user_id );
+      }
+    }
+
+
+
+    $SR =
+    [
+      'username_error' => $username_error,
+      'email_error' => $email_error
+    ];
+    return $SR;
+  }
+
+
 }
