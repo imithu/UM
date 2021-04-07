3974f9c8151b2a7f1aa5e3eb239272fa492e87c2
imithu <imithu@imithu.com>
2021-04-08 05:06:44 +0600

    fixed - datatype error

--- UM/Database/Users.php
+++ UM/Database/Users.php
@@ -8,75 +8,75 @@ use Illuminate\Support\Facades\DB;
 
 class Users
 {
-    /**
-     * 
-     * get user id by username
-     * 
-     * @param string $username
-     * 
-     * @return int $id
-     *             0 if not found
-     * 
-     * @since   0.0.0
-     * @version 1.0.0
-     * @author  Mahmudul Hasan Mithu
-     */
-    public static function id_username( string $username )
-    {
-        $id = DB::select('SELECT id FROM UM_users WHERE    `username`=? ORDER BY id DESC', [ htmlspecialchars(trim($username)) ])[0]->id ?? 0;
-        return $id;
+  /**
+   * 
+   * get user id by username
+   * 
+   * @param string $username
+   * 
+   * @return int $id
+   *             0 if not found
+   * 
+   * @since   0.0.0
+   * @version 1.11.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function id_username( string $username )
+  {
+    $id = DB::select('SELECT id FROM UM_users WHERE    `username`=? ORDER BY id DESC', [ htmlspecialchars(trim($username)) ])[0]->id ?? 0;
+    return (int) $id;
+  }
+
+
+  /**
+   * 
+   * get user id by email
+   * 
+   * @param string $email
+   * 
+   * @return int $id
+   *             0 if not found
+   * 
+   * @since   0.0.0
+   * @version 1.11.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function id_email( string $email )
+  {
+    $id = DB::select('SELECT id FROM UM_users WHERE    `email`=? ORDER BY id DESC', [ htmlspecialchars(trim($email)) ])[0]->id ?? 0;
+    return (int) $id;
+  }
+
+
+  /**
+   * 
+   * get user id by username or email
+   * 
+   * @param string $username_or_email
+   * 
+   * @return int $id
+   *             0 if not found
+   * 
+   * @since   0.0.0
+   * @version 1.11.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function id_username_or_email( string $username_or_email )
+  {
+    $user_id = self::id_username( $username_or_email );
+    
+    if( $user_id==0 ){
+      $user_id = self::id_email( $username_or_email );
     }
 
-
-    /**
-     * 
-     * get user id by email
-     * 
-     * @param string $email
-     * 
-     * @return int $id
-     *             0 if not found
-     * 
-     * @since   0.0.0
-     * @version 1.0.0
-     * @author  Mahmudul Hasan Mithu
-     */
-    public static function id_email( string $email )
-    {
-        $id = DB::select('SELECT id FROM UM_users WHERE    `email`=? ORDER BY id DESC', [ htmlspecialchars(trim($email)) ])[0]->id ?? 0;
-        return $id;
+    if( $user_id==0 ){
+      return (int) 0;
+    }elseif( $user_id>0 ){
+      return (int) $user_id;
     }
 
-
-    /**
-     * 
-     * get user id by username or email
-     * 
-     * @param string $username_or_email
-     * 
-     * @return int $id
-     *             0 if not found
-     * 
-     * @since   0.0.0
-     * @version 0.0.0
-     * @author  Mahmudul Hasan Mithu
-     */
-    public static function id_username_or_email( string $username_or_email )
-    {
-        $user_id = self::id_username( $username_or_email );
-        
-        if( $user_id==0 ){
-            $user_id = self::id_email( $username_or_email );
-        }
-
-        if( $user_id==0 ){
-            return 0;
-        }elseif( $user_id>0 ){
-            return $user_id;
-        }
-
-        return $user_id;
-    }
+    return (int) $user_id;
+  }
 
 
 
