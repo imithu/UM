0e34aa5b490bb2d8259bf8aaeb81c2f8cefd9acb
imithu <imithu@imithu.com>
2021-05-06 07:53:32 +0600

    update method is added

--- UM/User/Password.php
+++ UM/User/Password.php
@@ -3,6 +3,7 @@
 namespace UM\User;
 
 
+use Illuminate\Support\Facades\DB;
 use UM\Verify\User;
 use UM\Verify\Syntax;
 use UM\Generate\Unknown_Data;
@@ -11,6 +12,26 @@ use UM\Database\Users;
 
 class Password
 {
+
+
+  /**
+   * update user password
+   * 
+   * @param int    $id_user
+   * @param string $password
+   * 
+   * @since   2.0.0
+   * @version 2.0.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function update( int $id_user, string $password )
+  {
+    DB::table('UM_users')->where('id', $id_user)->update([ 'password' => password_hash($password, PASSWORD_DEFAULT) ]);
+  }
+
+
+
+
     /**
      * update password of an user
      * 
