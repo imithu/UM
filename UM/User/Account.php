3421ccca587459dea2397d16517ee5441d165db5
imithu <imithu@imithu.com>
2021-05-06 07:27:14 +0600

    is_exist is improved and renamed to get_id

--- UM/User/Account.php
+++ UM/User/Account.php
@@ -28,18 +28,19 @@ class Account
 
 
   /**
-   * check user exists or not
+   * get id_user by id_user, username, email
+   * also get the info of user existence
    * 
    * @param int|string id_user | username | email
    * 
-   * @return bool true  - user exists
-   *              false - user does not exists
+   * @return int >0  - user exists
+   *              0  - user does not exists
    * 
    * @since   2.0.0
    * @version 2.0.0
    * @author  Mahmudul Hasan Mithu
    */
-  public static function is_exist( $value )
+  public static function get_id( $value )
   {
     $id_user = DB::table('UM_users')
                 ->where( 'id', $value )
@@ -47,8 +48,8 @@ class Account
                 ->orWhere( 'email', $value )
                 ->value('id');
 
-    if( $id_user>0 ) return true;
-    return false;
+    if( $id_user>0 ) return $id_user;
+    return 0;
   }
 
 
