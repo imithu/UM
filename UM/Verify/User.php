2e3038917e686e7b11738056fa2128bf806a021e
imithu <imithu@imithu.com>
2021-05-06 06:51:55 +0600

    improved code of user_is_verified is moved in Account class

--- UM/Verify/User.php
+++ UM/Verify/User.php
@@ -35,30 +35,6 @@ class User{
         }
         return false;
     }
-
-
-
-
-    /**
-     * check a user is verified or not
-     * 
-     * @param int $user_id
-     * 
-     * @return bool - true  - user_id is     verified
-     *                false - user_id is not verified
-     * 
-     * 
-     * @since   1.0.0
-     * @version 1.0.0
-     * @author  Mahmudul Hasan Mithu
-     */
-    public static function user_is_verified( int $user_id )
-    {
-        if( Users::select( $user_id, 'email_is_verified' )=='yes' ){
-            return true;
-        }
-        return false;
-    }
 }
 
 
