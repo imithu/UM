cb3ab2fb6ff389cf1515adfd3b4ec41960760e7e
imithu <imithu@imithu.com>
2021-05-06 06:40:56 +0600

    is_exist is added

--- UM/User/Account.php
+++ UM/User/Account.php
@@ -1,24 +1,55 @@
 <?php
 namespace UM\User;
 
-use UM\Verify\User;
-use UM\Database\Users;
 use Illuminate\Support\Facades\DB;
 
 class Account
 {
-    /**
-     * Delete whole account based on user_id
-     * 
-     * @param int $user_id
-     * 
-     * @since   0.0.0
-     * @version 0.0.0
-     * @author  Mahmudul Hasan Mithu
-     */
-    public static function delete( int $user_id )
-    {
-        DB::delete('DELETE FROM UM_users WHERE `id`=?', [$user_id]);
-        DB::delete('DELETE FROM UM_usermeta WHERE `user_id`=?', [$user_id]);
-    }
+  /**
+   * Delete whole account based on user_id
+   * 
+   * @param int $user_id
+   * 
+   * @since   0.0.0
+   * @version 0.0.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function delete( int $user_id )
+  {
+    DB::delete('DELETE FROM UM_users WHERE `id`=?', [$user_id]);
+    DB::delete('DELETE FROM UM_usermeta WHERE `user_id`=?', [$user_id]);
+  }
+
+
+
+
+  /**
+   * check user exist or not
+   * 
+   * @param int|string id_user | username | email
+   * 
+   * @since   2.0.0
+   * @version 2.0.0
+   * @author  Mahmudul Hasan Mithu
+   */
+  public static function is_exist( $value )
+  {
+    $id_user = DB::table('UM_users')
+                ->where( 'id', $value )
+                ->orWhere( 'username', $value )
+                ->orWhere( 'email', $value )
+                ->value('id');
+
+    if( $id_user>0 ) return true;
+    return false;
+  }
+
+
+
+
+
+
+
+
+
 }
